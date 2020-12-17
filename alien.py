1074c33dead0f9e03a6f12e0f67808f60247ae76
PierreCPE <pierre.gosson@cpe.fr>
2020-12-17 09:26:05 +0100

    La daronne de clement

---
+++ alien.py
@@ -0,0 +1,22 @@
+#------------------Classe alien------------------------#
+class alien :
+    def __init__(self,cNumber,cPosition) :
+        self.__number=cNumber
+        self.__position=cPosition
+
+    def setPosition(self,newPosition):
+        self.__position=newPosition
+        #Place le vaisseau du joueur avec une méthode .coord
+
+
+    def getPosition(self):
+        return self.__position
+
+    def alienShoot(self):
+        #Crée un tir d'alien 
+
+    def bounceToRight(self):
+        #Rebond à droite
+
+    def bounceToLeft(self):
+        #Rebond à gauche
