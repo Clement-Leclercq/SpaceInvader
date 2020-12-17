5663f4b4614c466f5b8b7217933c463b65c07beb
Clement <leclercqcl@yahoo.fr>
2020-12-17 09:40:17 +0100

    ok

--- window.py
+++ window.py
@@ -12,10 +12,20 @@ TO DO: TOUT
 from tkinter import Tk, Button, Label, StringVar, Entry, Canvas, PhotoImage, NW, W, E, N, S
 from tkinter import messagebox #permet la boite de dialogue
 
-
-
+#Importation de nos classes
+from player import player #Classe player qui gère vie et score et position du vaisseau
+from alien import alien #Classe alien qui gère le nombre et la position de l'alien
 def play():
     print("test")
+
+"""
+Création du joueur
+"""
+score = 0
+lives = 3
+position = [450,800]
+
+player1 = player(score,lives,position)
 
 
 
@@ -33,7 +43,7 @@ quitButton = Button(spaceWindow, text = "Quit", command = spaceWindow.destroy)
 newButton = Button(spaceWindow, text = "New game", command = play)
 #Les label
 lives = StringVar ()
-lives.set("Lives: ")
+lives.set("Lives: "+player.getLife())
 score = StringVar ()
 score.set("Score:")
 livesLabel = Label(spaceWindow, textvariable = lives)
@@ -53,84 +63,6 @@ newButton.grid(row = 2, column = 2)
 quitButton.grid(row = 2, column = 2, sticky = S)
 
 spaceWindow.mainloop()
-
-
-
-#------------Classe joueur-----------------#
-#Classe joueur qui va nous donner et modifié son score, ses vies
-#Qui va aussi lui permettre de tirer et de se déplacer
-class player : 
-    def __init__(self,cScore,cLife,cPosition) :
-        self.__life=cLife
-        self.__score=cScore
-        self.__position=cPosition
-
-
-    def setLife(self,newlife):
-        self.__life=newlife
-
-
-    def getLife(self):
-        return self.__life
-
-    def setScore(self,newScore):
-        self.__score=newScore
-
-    def getScore(self):
-        return self.__score
-
-    def playerShoot(self):
-        #Faire ici la fonction tkinter ou quand on appui
-        #Sur espace ça tire
-
-    def goLeft(self):
-        #Le joueur va à gauche 
-
-    def goRight(self):
-        #Le joueur va à droite
-
-    def shipSize(self):
-        #Taille du vaisseau
-
-    def setPosition(self,newPosition):
-        self.__position=newPosition
-        #PLace le vaisseau du joueur avec une méthode .coord
-
-
-    def getPosition(self):
-        return self.__position
-
-
-
-
-#------------------Classe alien------------------------#
-class alien :
-    def __init__(self,cNumber,cPosition) :
-        self.__number=cNumber
-        self.__position=cPosition
-
-    def setPosition(self,newPosition):
-        self.__position=newPosition
-        #Place le vaisseau du joueur avec une méthode .coord
-
-
-    def getPosition(self):
-        return self.__position
-
-    def alienShoot(self):
-        #Crée un tir d'alien 
-
-    def bounceToRight(self):
-        #Rebond à droite
-
-    def bounceToLeft(self):
-        #Rebond à gauche
-
-    
-
-#---------Code-----------#
-
-#Une fonction 
 
 
 
