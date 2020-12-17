04e1d42c55b18c2350b58c83980a42007752c8c9
Clement <leclercqcl@yahoo.fr>
2020-12-17 09:21:39 +0100

    add window.py

--- fenetre.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-#Header
-"""
-Ce programme permet la création de la fenêtre
-Programme fait par Pierre Gosson et Clément Leclercq
-Fait le 17/12/2020
-TO DO: 
-"""
---
+++ window.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+#Header
+"""
+Ce programme permet la création de la fenêtre
+Programme fait par Pierre Gosson et Clément Leclercq
+Fait le 17/12/2020
+TO DO: TOUT
+"""
+
+#Importation des bibliothèques tkinter nécessaires
+from tkinter import Tk, Button, Label, StringVar, Entry, Canvas, PhotoImage, NW, W, E, N, S
+from tkinter import messagebox #permet la boite de dialogue
+
+
+
+def play():
+    print("test")
+
+
+
+"""
+Programme gérant la création de la fenêtre
+"""
+#Création de la fenêtre
+spaceWindow = Tk()
+spaceWindow.title("Space Invader")
+spaceWindow.geometry("980x690+200+100")
+
+#Création des différents éléments
+#Les boutons
+quitButton = Button(spaceWindow, text = "Quit", command = spaceWindow.destroy)
+newButton = Button(spaceWindow, text = "New game", command = play)
+#Les label
+lives = StringVar ()
+lives.set("Lives: ")
+score = StringVar ()
+score.set("Score:")
+livesLabel = Label(spaceWindow, textvariable = lives)
+scoreLabel = Label(spaceWindow, textvariable = score)
+#Le Canvas => penser à mettre le vaisseau
+picture = PhotoImage(file="picture/background.gif")
+x = 900
+y = 900
+spaceCanvas = Canvas(spaceWindow, width = x, height = y)
+spaceCanvas.create_image(0,0,anchor=NW, image = picture)
+
+#Agencement dans la fenêtre
+scoreLabel.grid(row = 1, column = 1, sticky = W)
+livesLabel.grid(row = 1, column = 1, sticky = E)
+spaceCanvas.grid(row = 2, column = 1, rowspan = 2)
+newButton.grid(row = 2, column = 2)
+quitButton.grid(row = 2, column = 2, sticky = S)
+
+spaceWindow.mainloop()
+
